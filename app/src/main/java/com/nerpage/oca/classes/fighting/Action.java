e3d55adb1198fc8ef41918f7fee5f00b3eca17a0
Neripheral <neripheral@gmail.com>
2021-04-03 08:16:23 +0200

    mod: make Action targeted

--- app/src/main/java/com/nerpage/oca/classes/fighting/Action.java
+++ app/src/main/java/com/nerpage/oca/classes/fighting/Action.java
@@ -1,5 +1,40 @@
 package com.nerpage.oca.classes.fighting;
 
+import com.nerpage.oca.classes.Entity;
+
 public abstract class Action {
+    //================================================================================
+    // region //            Fields
+
+    private Entity source;
+    private Entity target;
+
+    // endregion //         Fields
+    //================================================================================
+    //================================================================================
+    // region //            Accessors
+
+    public Entity getSource() {
+        return source;
+    }
+
+    public Action setSource(Entity source) {
+        this.source = source;
+        return this;
+    }
+
+    public Entity getTarget() {
+        return target;
+    }
+
+    public Action setTarget(Entity target) {
+        this.target = target;
+        return this;
+    }
+
+
+    // endregion //         Accessors
+    //================================================================================
+
     public abstract Status getAppliedStatus();
 }
