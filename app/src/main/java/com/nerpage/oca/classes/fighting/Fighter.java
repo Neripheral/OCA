f7a680205b838d2490d108ee8355c3aa85142780
Neripheral <neripheral@gmail.com>
2021-04-04 03:31:56 +0200

    feat: expand Fighter with pending action and logic

--- app/src/main/java/com/nerpage/oca/classes/fighting/Fighter.java
+++ app/src/main/java/com/nerpage/oca/classes/fighting/Fighter.java
@@ -3,7 +3,17 @@ package com.nerpage.oca.classes.fighting;
 import com.nerpage.oca.classes.Entity;
 
 public class Fighter {
+    //================================================================================
+    // region //            Fields
+
     private Entity entity;
+    private Action pendingAction;
+    private FightingBehavior behavior;
+
+    // endregion //         Fields
+    //================================================================================
+    //================================================================================
+    // region //            Accessors
 
     public Entity getEntity() {
         return entity;
@@ -14,7 +24,34 @@ public class Fighter {
         return this;
     }
 
+    public Action getPendingAction() {
+        return pendingAction;
+    }
+
+    public Fighter setPendingAction(Action pendingAction) {
+        this.pendingAction = pendingAction;
+        return this;
+    }
+
+    public FightingBehavior getBehavior() {
+        return behavior;
+    }
+
+    public Fighter setBehavior(FightingBehavior behavior) {
+        this.behavior = behavior;
+        return this;
+    }
+
+    // endregion //         Accessors
+    //================================================================================
+    //================================================================================
+    // region //            Constructors
+
     public Fighter(Entity entity){
         this.setEntity(entity);
     }
+
+    // endregion //         Constructors
+    //================================================================================
+
 }
