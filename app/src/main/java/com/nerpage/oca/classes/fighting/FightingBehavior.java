1ae06530c7f426f556a525022afccc360b4b32a9
Neripheral <neripheral@gmail.com>
2021-04-04 06:10:58 +0200

    wip: simplify fighting behavior

--- app/src/main/java/com/nerpage/oca/classes/fighting/FightingBehavior.java
+++ app/src/main/java/com/nerpage/oca/classes/fighting/FightingBehavior.java
@@ -1,16 +1,9 @@
 package com.nerpage.oca.classes.fighting;
 
 import java.util.List;
-import java.util.function.BiFunction;
 
-public class FightingBehavior {
-    private BiFunction<Fighter, List<Fighter>, Action> logic;
+public abstract class FightingBehavior {
+    public abstract Action promptAction(Fighter host, List<Fighter> others);
 
-    public Action promptAction(Fighter host, List<Fighter> others){
-        return this.logic.apply(host, others);
-    }
-
-    public FightingBehavior(){
-        this.logic = ((host, others) -> null);
-    }
+    public FightingBehavior(){}
 }
