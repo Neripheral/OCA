5622b600c69ea674af7c7e1919370e8fa9ee6b2a
Neripheral <neripheral@gmail.com>
2021-04-04 07:20:49 +0200

    feat: add EnemyGenerator

--- app/src/main/java/com/nerpage/oca/classes/fighting/EnemyGenerator.java
+++ app/src/main/java/com/nerpage/oca/classes/fighting/EnemyGenerator.java
@@ -2,9 +2,13 @@ package com.nerpage.oca.classes.fighting;
 
 import com.nerpage.oca.classes.Entity;
 import com.nerpage.oca.classes.entities.TrainingDummy;
+import com.nerpage.oca.classes.fighting.behaviors.BraindeadFightingBehavior;
 
 public class EnemyGenerator {
-    public static Entity generate(){
-        return new TrainingDummy();
+    public static Fighter generateRandomEnemy(){
+        return new Fighter(
+                new TrainingDummy(),
+                new BraindeadFightingBehavior()
+        );
     }
 }
