7387a774831643e5ae096f5a58d218df3ed22fd4
Neripheral <neripheral@gmail.com>
2021-04-11 18:28:16 +0200

    feat: extract interface HasLayout from LayoutHelper
    Now Layouts that extends other layouts can use some LayoutHelper
    features as well.

--- app/src/main/java/com/nerpage/oca/classes/LayoutHelper.java
+++ app/src/main/java/com/nerpage/oca/classes/LayoutHelper.java
@@ -4,14 +4,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public abstract class LayoutHelper {
-    /**
-     * POI represents each View in Layout that is important.
-     * As such each final LayoutHelper should have own enum implementing POI.
-     */
-    public interface POI{
-        int getId();
-    }
+import com.nerpage.oca.interfaces.HasLayout;
+
+public abstract class LayoutHelper implements HasLayout {
+    public interface POI extends HasLayout.POI{}
 
     //================================================================================
     // Fields
@@ -36,33 +32,7 @@ public abstract class LayoutHelper {
     // Methods
     //================================================================================
 
-    private View getView(int id){
-        return this.getRoot().findViewById(id);
-    }
 
-    public View getView(POI poi){
-        return this.getView(poi.getId());
-    }
-
-    public TextView updateText(POI poi, String text){
-        ((TextView)this.getView(poi)).setText(text);
-        return ((TextView)this.getView(poi));
-    }
-
-    public ImageView updateImage(POI poi, int imgId){
-        ((ImageView)this.getView(poi)).setImageResource(imgId);
-        return ((ImageView)this.getView(poi));
-    }
-
-    public View show(POI poi){
-        this.getView(poi).setVisibility(View.VISIBLE);
-        return this.getView(poi);
-    }
-
-    public View hide(POI poi){
-        this.getView(poi).setVisibility(View.GONE);
-        return this.getView(poi);
-    }
 
     //================================================================================
     // Constructors
