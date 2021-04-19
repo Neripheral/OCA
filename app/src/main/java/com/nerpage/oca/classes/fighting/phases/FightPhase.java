ef5f20d012230c51df99d65e10e7f740feee9222
Neripheral <neripheral@gmail.com>
2021-04-19 04:17:20 +0200

    feat: init FightPhase

---
+++ app/src/main/java/com/nerpage/oca/classes/fighting/phases/FightPhase.java
@@ -0,0 +1,14 @@
+package com.nerpage.oca.classes.fighting.phases;
+
+import com.nerpage.oca.classes.fighting.Fight;
+
+public abstract class FightPhase {
+    public final Fight fight;
+
+    public abstract FightPhase getNextPhase();
+    public abstract void execute();
+
+    public FightPhase(Fight fight) {
+        this.fight = fight;
+    }
+}
