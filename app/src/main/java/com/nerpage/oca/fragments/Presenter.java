2a90f3f35b37e581aa1a0695955314e777a697f9
Neripheral <neripheral@gmail.com>
2022-03-10 05:37:45 +0100

    feat: playEffect method

--- app/src/main/java/com/nerpage/oca/fragments/Presenter.java
+++ app/src/main/java/com/nerpage/oca/fragments/Presenter.java
@@ -1,7 +1,10 @@
 package com.nerpage.oca.fragments;
 
 import android.view.View;
+import android.widget.ImageView;
 
+import com.nerpage.oca.classes.helpers.AnimationHelper;
+import com.nerpage.oca.layouts.BattlegroundLayout;
 import com.nerpage.oca.layouts.HasLayout;
 
 public abstract class Presenter implements HasLayout {
@@ -45,6 +48,12 @@ public abstract class Presenter implements HasLayout {
 
     public boolean isRootNull(){
         return (getRoot() == null);
+    }
+
+    public void playEffect(POI poi, int resId, int duration, float scale, Runnable after){
+        getView(poi).setScaleX(scale);
+        getView(poi).setScaleY(scale);
+        AnimationHelper.playCustomDurationAnimation((ImageView)getView(poi), resId, duration, after);
     }
 
     // endregion //         Interface
