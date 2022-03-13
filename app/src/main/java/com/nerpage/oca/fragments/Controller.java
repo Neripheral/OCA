cc5fbbb70726ffe797174732ad7f0d4e5220fcf1
Neripheral <neripheral@gmail.com>
2022-03-13 08:22:22 +0100

    mod: remove Context from default constructor

--- app/src/main/java/com/nerpage/oca/fragments/Controller.java
+++ app/src/main/java/com/nerpage/oca/fragments/Controller.java
@@ -1,20 +1,25 @@
 package com.nerpage.oca.fragments;
 
 import android.content.Context;
+import android.view.View;
 
 public abstract class Controller<M extends Model, P extends Presenter> {
     protected final M m;
     protected final P p;
 
-    private final Context context;
+    private Context context;
 
     protected Context getContext(){
         return context;
     }
 
-    Controller(M newM, P newP, Context newContext){
+    public void setRoot(View newRoot){
+        context = newRoot.getContext();
+        p.setRoot(newRoot);
+    }
+
+    Controller(M newM, P newP){
         m = newM;
         p = newP;
-        context = newContext;
     }
 }
