314e5ef4a173c78e7e0ce8c9dbfc86969030d5cf
Neripheral <neripheral@gmail.com>
2022-03-18 16:37:51 +0100

    init: FighterCardModel

---
+++ app/src/main/java/com/nerpage/oca/fragments/models/FighterCardModel.java
@@ -0,0 +1,9 @@
+package com.nerpage.oca.fragments.models;
+
+import com.nerpage.oca.fragments.Model;
+
+public class FighterCardModel extends Model {
+    public String title;
+    public String currentBlood;
+    public String maxBlood;
+}
