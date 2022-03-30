504cbbe704b3381195552c2d67a35d03ba6c958d
Neripheral <neripheral@gmail.com>
2022-03-30 12:20:47 +0200

    init: PAC ActionCardModel

---
+++ app/src/main/java/com/nerpage/oca/fragments/models/ActionCardModel.java
@@ -0,0 +1,9 @@
+package com.nerpage.oca.fragments.models;
+
+import com.nerpage.oca.fragments.Model;
+
+public class ActionCardModel extends Model {
+    public String title;
+    public int thumbnailResId;
+    String description;
+}
