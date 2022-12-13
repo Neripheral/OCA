793d28ef4318bb18ec4e9cf934c903ced32e9de1
Neripheral <neripheral@gmail.com>
2022-12-13 19:18:18 +0100

    nit: remove comment bloat from Presenter

--- app/src/main/java/com/nerpage/oca/fragments/Presenter.java
+++ app/src/main/java/com/nerpage/oca/fragments/Presenter.java
@@ -7,39 +7,19 @@ import com.nerpage.oca.classes.helpers.AnimationHelper;
 import com.nerpage.oca.layouts.HasLayout;
 
 public abstract class Presenter implements HasLayout {
-    //================================================================================
-    // region //            POI
 
     public interface POI extends HasLayout.POI{}
 
-    // endregion //         POI
-    //================================================================================
-    //================================================================================
-    // region //            Fields
+
 
     private final View root;
-
-    // endregion //         Fields
-    //================================================================================
-    //================================================================================
-    // region //            Accessors
 
     @Override
     public View getRoot() {
         return root;
     }
 
-    // endregion //         Accessors
-    //================================================================================
-    //================================================================================
-    // region //            Private methods
 
-
-
-    // endregion //         Private methods
-    //================================================================================
-    //================================================================================
-    // region //            Interface
 
     public void playEffect(POI poi, int resId, int duration, float scale, Runnable after){
         getView(poi).setScaleX(scale);
@@ -47,15 +27,10 @@ public abstract class Presenter implements HasLayout {
         AnimationHelper.playCustomDurationAnimation((ImageView)getView(poi), resId, duration, after);
     }
 
-    // endregion //         Interface
-    //================================================================================
-    //================================================================================
-    // region //            Constructors
+
 
     public Presenter(View newRoot){
         root = newRoot;
     }
 
-    // endregion //         Constructors
-    //================================================================================
 }
