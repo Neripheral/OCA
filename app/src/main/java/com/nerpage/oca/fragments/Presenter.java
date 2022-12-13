4d8a7533fcb17a75a279feb8b51d3c59ade4c594
Neripheral <neripheral@gmail.com>
2022-12-13 20:10:26 +0100

    mod: remove Presenter dependency on obsolete HasLayout

--- app/src/main/java/com/nerpage/oca/fragments/Presenter.java
+++ app/src/main/java/com/nerpage/oca/fragments/Presenter.java
@@ -2,19 +2,20 @@ package com.nerpage.oca.fragments;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.nerpage.oca.classes.helpers.AnimationHelper;
-import com.nerpage.oca.layouts.HasLayout;
 
-public abstract class Presenter implements HasLayout {
+public abstract class Presenter {
 
-    public interface POI extends HasLayout.POI{}
+    public interface POI{
+        int getId();
+    }
 
 
 
     private final View root;
 
-    @Override
     public View getRoot() {
         return root;
     }
@@ -27,7 +28,29 @@ public abstract class Presenter implements HasLayout {
         AnimationHelper.playCustomDurationAnimation((ImageView)getView(poi), resId, duration, after);
     }
 
+    public View getView(POI poi) {
+        return getRoot().findViewById(poi.getId());
+    }
 
+    public TextView updateText(POI poi, String text){
+        ((TextView)this.getView(poi)).setText(text);
+        return ((TextView)this.getView(poi));
+    }
+
+    public ImageView updateImage(POI poi, int imgId){
+        ((ImageView)this.getView(poi)).setImageResource(imgId);
+        return ((ImageView)this.getView(poi));
+    }
+
+    public View show(POI poi){
+        this.getView(poi).setVisibility(View.VISIBLE);
+        return this.getView(poi);
+    }
+
+    public View hide(POI poi){
+        this.getView(poi).setVisibility(View.GONE);
+        return this.getView(poi);
+    }
 
     public Presenter(View newRoot){
         root = newRoot;
