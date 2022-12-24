59bcd6ea04842b688070d7d85714d505e0040ff2
Neripheral <neripheral@gmail.com>
2022-12-24 04:25:58 +0100

    feat: implement POI.of(root) method

--- app/src/main/java/com/nerpage/oca/fragments/Presenter.java
+++ app/src/main/java/com/nerpage/oca/fragments/Presenter.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import com.nerpage.oca.classes.helpers.AnimationHelper;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public abstract class Presenter {
 
@@ -37,6 +38,19 @@ public abstract class Presenter {
          * @return resource id (R.id.*)
          */
         int getId();
+
+        /**
+         * Utility method that fetches {@code View} that this POI represents based on {@code View}
+         * hierarchy with given root.
+         * @param root root of the hierarchy client needs to find {@code View} for.
+         *             Throws {@code NullPointerException} if null.
+         * @return {@code Optional} result. Present if there is a {@code View} for given root,
+         *          empty if there isn't.
+         */
+        default Optional<View> of(@NonNull View root){
+            Objects.requireNonNull(root, "Argument root must not be null");
+            return Optional.ofNullable(root.findViewById(getId()));
+        }
     }
 
     /**
