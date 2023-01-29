322c945417da6ba8862eb68f5986a09349006b88
Neripheral <neripheral@gmail.com>
2023-01-30 00:25:19 +0100

    test: add retain state test that fails

--- app/src/androidTest/java/com/nerpage/oca/fragments/ExampleNodeTest.java
+++ app/src/androidTest/java/com/nerpage/oca/fragments/ExampleNodeTest.java
@@ -6,14 +6,21 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static com.google.common.truth.Truth.assertThat;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.view.View;
+
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.lifecycle.Lifecycle;
 import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.action.ViewActions;
 
 import com.nerpage.oca.R;
 import com.nerpage.oca.pac.presenters.ExamplePresenter;
 
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -54,10 +61,18 @@ class ExampleNodeTest {
     @Test
     public void titleChangesCorrectlyWhenConfirmButtonPressed() {
         scenario.moveToState(Lifecycle.State.RESUMED);
-        onView(withId(R.id.f_example_inputText)).perform(ViewActions.typeText("Success"));
+        onView(withId(R.id.f_example_inputText)).perform(ViewActions.typeText("New Title"));
         onView(withId(R.id.f_example_confirmButton)).perform(ViewActions.click());
-        onView(withId(R.id.f_example_title)).check(matches(withText("Success")));
+        onView(withId(R.id.f_example_title)).check(matches(withText("New Title")));
     }
 
-
+    @Test
+    public void titleRemainsAfterStateChange(){
+        scenario.moveToState(Lifecycle.State.RESUMED);
+        onView(withId(R.id.f_example_inputText)).perform(ViewActions.typeText("New Title"));
+        onView(withId(R.id.f_example_confirmButton)).perform(ViewActions.click());
+        scenario.moveToState(Lifecycle.State.CREATED);
+        scenario.moveToState(Lifecycle.State.RESUMED);
+        onView(withId(R.id.f_example_title)).check(matches(withText("New Title")));
+    }
 }
