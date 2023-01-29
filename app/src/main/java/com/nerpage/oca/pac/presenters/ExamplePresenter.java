5132e4d87e72da9e736a943ef1c5c6b94bf02445
Neripheral <neripheral@gmail.com>
2023-01-29 04:07:20 +0100

    docs: create ExamplePresenter documentation

--- app/src/main/java/com/nerpage/oca/pac/presenters/ExamplePresenter.java
+++ app/src/main/java/com/nerpage/oca/pac/presenters/ExamplePresenter.java
@@ -4,16 +4,35 @@ import com.nerpage.oca.R;
 import com.nerpage.oca.pac.Presenter;
 
 public interface ExamplePresenter extends Presenter {
+    /**
+     * Returns the resource id of the layout this presenter describes
+     * @return id
+     */
     static int getDescribedLayoutId() {
         return R.layout.fragment_example;
     }
 
+    /**
+     * Changes the title text to a new value.
+     * @param newTitle new {@code String} value for title
+     */
     void setTitle(String newTitle);
 
+    /**
+     * Fetches text currently present in the input field.
+     * @return the {@code String} text
+     */
     String getInputFieldText();
 
+    /**
+     * Sets a new callback for notifying when the confirm button has been pressed.
+     * @param newCallback new callback
+     */
     void setOnConfirmButtonPressedCallback(Callback newCallback);
 
+    /**
+     * {@code POI} specifically designed to reference {@code ExamplePresenter}'s layout key views
+     */
     enum POI implements Presenter.PointOfInterest {
         ROOT(R.id.root_example),
         TITLE(R.id.f_example_title),
