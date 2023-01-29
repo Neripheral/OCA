a53ec37f6a08df9c412a9922e35c66bb33d76d95
Neripheral <neripheral@gmail.com>
2023-01-29 05:02:31 +0100

    feat: init AbstractController

---
+++ app/src/main/java/com/nerpage/oca/pac/AbstractController.java
@@ -0,0 +1,41 @@
+package com.nerpage.oca.pac;
+
+import androidx.fragment.app.Fragment;
+
+import com.nerpage.oca.fragments.Model;
+
+import java.util.Optional;
+
+public abstract class AbstractController<M extends Model, P extends Presenter> extends Fragment {
+    private Presenter.AbstractFactory<P> presenterFactory;
+
+    protected void setPresenterFactory(Presenter.AbstractFactory<P> presenterFactory){
+        this.presenterFactory = presenterFactory;
+    }
+
+    protected Optional<? extends Presenter.AbstractFactory<P>> getPresenterFactory(){
+        return Optional.ofNullable(presenterFactory);
+    }
+
+
+    private P presenter;
+
+    protected Optional<P> getPresenter() {
+        return Optional.ofNullable(presenter);
+    }
+
+    protected void setPresenter(P presenter) {
+        this.presenter = presenter;
+    }
+
+
+    private M model;
+
+    protected Optional<M> getModel(){
+        return Optional.ofNullable(model);
+    }
+
+    protected void setModel(M model){
+        this.model = model;
+    }
+}
