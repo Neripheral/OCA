dd082daff657723ecb06e44be2fa173630288eb3
Neripheral <neripheral@gmail.com>
2023-01-30 03:06:32 +0100

    doc: create AbstractController documentation

--- app/src/main/java/com/nerpage/oca/pac/AbstractController.java
+++ app/src/main/java/com/nerpage/oca/pac/AbstractController.java
@@ -3,35 +3,78 @@ package com.nerpage.oca.pac;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Abstraction of the controller of a node. Responsible for data transfers and logic.
+ * Uses {@code Controller}'s specific {@code Model} class and {@code Presenter} class.
+ * @param <M> {@code Model} responsible for handling node's data storage.
+ * @param <P> {@code Presenter} responsible for handling node's ui manipulation.
+ */
 public abstract class AbstractController<M extends Model, P extends Presenter> extends Fragment {
+    /**
+     * {@code Factory} responsible for creating this node's {@code Presenter}s when needed.
+     * If null {@code Presenter}s will not be created.
+     * For DI and decoupling purposes {@code Presenter} should be initialised via {@code Factory}.
+     */
+    @Nullable
     private Presenter.Factory<P> presenterFactory;
 
-    public void setPresenterFactory(Presenter.Factory<P> presenterFactory){
+    /**
+     * Setter changing this {@code Controller}'s {@code Presenter Factory}.
+     * @param presenterFactory new {@code Presenter Factory}. May be null.
+     */
+    public void setPresenterFactory(@Nullable Presenter.Factory<P> presenterFactory){
         this.presenterFactory = presenterFactory;
     }
 
+    /**
+     * Returns this {@code Controller}'s {@code Presenter Factory}. May be null thus the return
+     * value should be checked before using.
+     * @return {@code Optional} of {@code Factory}. Present if {@code this} has one.
+     */
     protected Optional<? extends Presenter.Factory<P>> getPresenterFactory(){
         return Optional.ofNullable(presenterFactory);
     }
 
 
+
+    /**
+     * {@code Presenter} responsible for handling UI changes for this node.
+     * If null, no changes to the UI will be made.
+     */
+    @Nullable
     private P presenter;
 
+    /**
+     * Returns this {@code Controller}'s {@code Presenter}. May be null thus the return
+     * value should be checked before using.
+     * @return {@code Optional} of {@code Presenter}. Present if {@code this} has one.
+     */
     protected Optional<P> getPresenter() {
         return Optional.ofNullable(presenter);
     }
 
+    /**
+     * Returns this {@code Controller}'s {@code Presenter} or throws {@code InvalidStateException}
+     * if it doesn't exist. Use only if absolutely sure the {@code Presenter} exists and the lack
+     * of thereof should mean code malfunctions.
+     * @return {@code Presenter} of this node.
+     */
     protected P requirePresenter(){
         return getPresenter().orElseThrow(
                 ()->new IllegalStateException("Presenter required but isn't available."));
     }
 
-    protected void setPresenter(P presenter) {
+    /**
+     * Setter changing this {@code Controller}'s responsible {@code Presenter}.
+     * @param presenter new {@code Presenter}. May be null.
+     */
+    protected void setPresenter(@Nullable P presenter) {
         this.presenter = presenter;
     }
 
@@ -52,17 +95,36 @@ public abstract class AbstractController<M extends Model, P extends Presenter> e
     }
 
 
+    /**
+     * {@code Model} responsible for storing node's data and informing {@code Controller} of
+     * any data changes. If null, no data will be saved or read.
+     */
     private M model;
 
+    /**
+     * Returns this {@code Controller}'s {@code Model}. May be null thus the return
+     * value should be checked before using.
+     * @return {@code Optional} of {@code Model}. Present if {@code this} has one.
+     */
     protected Optional<M> getModel(){
         return Optional.ofNullable(model);
     }
 
+    /**
+     * Returns this {@code Controller}'s {@code Model} or throws {@code InvalidStateException}
+     * if it doesn't exist. Use only if absolutely sure the {@code Model} exists and the lack
+     * of thereof should mean code malfunctions.
+     * @return {@code Model} of this node.
+     */
     protected M requireModel(){
         return getModel().orElseThrow(
                 ()->new IllegalStateException("Model required but isn't available."));
     }
 
+    /**
+     * Setter changing this {@code Controller}'s responsible {@code Model}.
+     * @param model new {@code Model}. May be null.
+     */
     protected void setModel(M model){
         this.model = model;
     }
