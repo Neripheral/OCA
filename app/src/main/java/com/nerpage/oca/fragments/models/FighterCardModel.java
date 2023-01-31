4da6d682ab32049f09cbc9d675a69c31402c8b4d
Neripheral <neripheral@gmail.com>
2023-01-31 01:12:45 +0100

    rem: remove old FighterCard related classes

--- app/src/main/java/com/nerpage/oca/fragments/models/FighterCardModel.java
+++
@@ -1,9 +0,0 @@
-package com.nerpage.oca.fragments.models;
-
-import com.nerpage.oca.fragments.Model;
-
-public class FighterCardModel extends Model {
-    public String title;
-    public String currentBlood;
-    public String maxBlood;
-}
