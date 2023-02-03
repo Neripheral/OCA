6446cc45f4fd851c7459417cf7bd67178c2b7334
Neripheral <neripheral@gmail.com>
2023-02-03 13:36:29 +0100

    test: implement ActionCardNodeTest basic data visibility tests

--- app/src/androidTest/java/com/nerpage/oca/fragments/ActionCardNodeTest.java
+++ app/src/androidTest/java/com/nerpage/oca/fragments/ActionCardNodeTest.java
@@ -1,15 +1,23 @@
 package com.nerpage.oca.fragments;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.lifecycle.Lifecycle;
 
 import com.nerpage.oca.R;
 import com.nerpage.oca.pac.controllers.ActionCardController;
-import com.nerpage.oca.pac.controllers.ExampleController;
 import com.nerpage.oca.pac.controllers.implementation.DefaultActionCardController;
+import com.nerpage.oca.pac.presenters.ActionCardPresenter;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 public class ActionCardNodeTest {
     public FragmentScenario<? extends ActionCardController> scenario;
@@ -23,9 +31,17 @@ public class ActionCardNodeTest {
                 Lifecycle.State.INITIALIZED);
     }
 
-    @Test
-    public void allViewsAreVisibleAtStart(){
+    @ParameterizedTest
+    @EnumSource(value = ActionCardPresenter.POI.class, names = {"TITLE", "THUMBNAIL", "DESCRIPTION", "INFO_BOX"})
+    public void dataIsNotVisibleWhenJustInitiated(ActionCardPresenter.POI poi){
+        onView(withId(poi.getId())).check(doesNotExist());
+    }
 
+    @ParameterizedTest
+    @EnumSource(value = ActionCardPresenter.POI.class, names = {"TITLE", "THUMBNAIL", "DESCRIPTION", "INFO_BOX"})
+    public void allViewsAreVisibleAtStart(ActionCardPresenter.POI poi){
+        scenario.moveToState(Lifecycle.State.RESUMED);
+        onView(withId(poi.getId())).check(matches(isDisplayed()));
     }
 
     @Test
